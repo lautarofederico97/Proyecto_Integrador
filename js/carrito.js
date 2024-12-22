8a1be0105d7e8c13dd4cded92ae89d469ec9d046
lautarofederico97 <lautarofederico97@gmail.com>
2024-12-22 08:48:12 -0300

    parche V2.1.0.0

--- js/carrito.js
+++ js/carrito.js
@@ -1,4 +1,18 @@
 let carrito = [];
+// Inicializamos el contador del carrito
+let cartCount = 0;
+
+
+//cargar productos en la pagina principal
+/*Cargar los productos dinámicamente desde el archivo productos.html */
+fetch('./main/productos.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('productos-container').innerHTML = data;
+        })
+    .catch(error => {
+            console.error('Error cargando los productos:', error);
+        }); 
 
 // Función para agregar un producto al carrito
 function agregarAlCarrito(nombre, precio) {
@@ -6,8 +20,20 @@ function agregarAlCarrito(nombre, precio) {
         nombre: nombre,
         precio: precio,
         cantidad: 1
-    };
+    }
+    cartCount++;
 
+    // Actualizamos el contador en el navbar
+    document.getElementById("cart-count").textContent = cartCount;
+
+    // Mostrar el popup de notificación
+    const popupNotification = document.getElementById("popup-notification");
+    popupNotification.classList.add("show");
+
+    // Ocultamos el popup después de 2 segundos
+    setTimeout(function() {
+        popupNotification.classList.remove("show");
+    }, 1000);
     // Verificar si el producto ya está en el carrito
     const index = carrito.findIndex(item => item.nombre === nombre);
     if (index > -1) {
@@ -74,6 +100,26 @@ document.addEventListener('DOMContentLoaded', () => {
     cargarCarrito(); // Llama a cargarCarrito cuando el DOM esté listo
 });
 
+
+
+
+// // Función para agregar un producto al carrito
+// document.getElementById("add-to-cart-btn").addEventListener("click", function() {
+//   // Incrementamos el contador del carrito
+//   cartCount++;
+
+//   // Actualizamos el contador en el carrito
+//   document.getElementById("cart-count").textContent = cartCount;
+
+//   // Mostramos el mensaje de notificación
+//   const notification = document.getElementById("notification");
+//   notification.style.display = "block";
+
+//   // Ocultamos la notificación después de 2 segundos
+//   setTimeout(function() {
+//     notification.style.display = "none";
+//   }, 2000);
+// });
 
 // Cargar el carrito al iniciar la página
 window.onload = cargarCarrito;
